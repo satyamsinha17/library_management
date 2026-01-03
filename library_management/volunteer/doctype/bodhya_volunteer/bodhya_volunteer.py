0356f05e6adc2f04927b17fcba828af6e934c57b
satyamsinha17 <satyamsinha667@gmail.com>
2026-01-03 12:06:20 +0530

    feat: Add volunteer management module with custom fields and permissions

---
+++ library_management/volunteer/doctype/bodhya_volunteer/bodhya_volunteer.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, satyma and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class BodhyaVolunteer(Document):
+	pass
